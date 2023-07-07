0638d097813249e676f4a548d3f7bc0630f47851
Karen Dantas <94451993+karendantas@users.noreply.github.com>
2023-07-07 11:15:34 -0300

    responsividade mobile 85% feita

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,10 @@
-const mobileBtn = document.getElementById('mobile-btn');
+const mobileBtn = document.getElementById('mobilebtn');
 
-function toggleMenu(event){
-    const navi = document.getElementbyId('navigation');
+function toggleMenu(){
+    const navi = document.getElementById('navigation');
+  
     navi.classList.toggle('active'); //o toggle adiciona a classe "active" ao clicarmos no botao, caso nao tenha
     //a classe, e remove a mesma após clicar novamente.
-    const active = navi.classList.contains('active');
+    
 }
 mobileBtn.addEventListener('click', toggleMenu);//criando um evento que ao clicar no botao, aciona  a função toggleMenu
