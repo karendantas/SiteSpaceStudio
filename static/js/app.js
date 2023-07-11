9b0888560858711a827885c588b0427871526c5e
Karen Dantas <94451993+karendantas@users.noreply.github.com>
2023-07-11 20:23:11 -0300

    ARRUMEI OS INPUUUUUT

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ function toggleMenu(){
     const navi = document.getElementById('navigation');
   
     navi.classList.toggle('active'); //o toggle adiciona a classe "active" ao clicarmos no botao, caso nao tenha
-    //a classe, e remove a mesma após clicar novamente.
     
+    //a classe, e remove a mesma após clicar novamente.
 }
 mobileBtn.addEventListener('click', toggleMenu);//criando um evento que ao clicar no botao, aciona  a função toggleMenu
