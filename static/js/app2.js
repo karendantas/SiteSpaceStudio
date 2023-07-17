b5faf826845490a4d2e09f6b02f87ec0ba38aea4
Karen Dantas <94451993+karendantas@users.noreply.github.com>
2023-07-17 14:23:02 -0300

    ajeitando mais coisas

--- static/js/app2.js
+++ static/js/app2.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-// const openModalButton = document.querySelector("#open-modal");
-// const closeModalButton = document.querySelector("#close-modal");
-// const modal = document.querySelector("#modal");
-// const fade = document.querySelector("#fade");
-// const toggleModal = () => {
-//     modal.classList.toggle("hide")
-//     fade.classList.toggle("hide")
-// }
-// [openModalButton, closeModalButton, fade].forEach((el) => {
-//     el.addEventListener("click", () => toggleModal())
-// })
 
 //logincadastro
 var login = document.querySelector("#log");
